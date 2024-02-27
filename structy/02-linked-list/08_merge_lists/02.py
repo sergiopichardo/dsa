392e8177db51b493660c7b46b2a2ab7977d75b20
Sergio Pichardo <sergiopichardo@users.noreply.github.com>
2024-02-27 10:47:35 -0600

    add recursive solution for merge lists exercise

--- structy/02-linked-list/08_merge_lists/02.py
+++ structy/02-linked-list/08_merge_lists/02.py
@@ -100,6 +100,31 @@ def merge_lists(head_1: Node, head_2: Node):
 
     return dummy_node.next
 
+
+def merge_lists_recursive(head_1: Node, head_2: Node):
+    # base case 
+    if head_1 is None and head_2 is None:
+        return None
+    
+    # logic 
+    if head_1 is None:
+        return head_2
+    
+    if head_2 is None:
+        return head_1
+    
+    head_2_next = head_2.next
+
+    if head_1.val < head_2.val:
+        head_1_next = head_1.next
+        head_1.next = merge_lists_recursive(head_1_next, head_2)
+        return head_1
+    else:
+        head_2_next = head_2.next
+        head_2.next = merge_lists_recursive(head_1, head_2_next)
+        return head_2
+
+
 def print_ll(node: Node):
     result = []
 
@@ -134,48 +159,48 @@ r.next = s
 s.next = t
 # 6 -> 8 -> 9 -> 25
 
-print_ll(merge_lists(a, q))
+print_ll(merge_lists_recursive(a, q))
 # 5 -> 6 -> 7 -> 8 -> 9 -> 10 -> 12 -> 20 -> 25 -> 28 
 
 
 
 
-a = Node(5)
-b = Node(7)
-c = Node(10)
-d = Node(12)
-e = Node(20)
-f = Node(28)
-a.next = b
-b.next = c
-c.next = d
-d.next = e
-e.next = f
-# 5 -> 7 -> 10 -> 12 -> 20 -> 28
+# a = Node(5)
+# b = Node(7)
+# c = Node(10)
+# d = Node(12)
+# e = Node(20)
+# f = Node(28)
+# a.next = b
+# b.next = c
+# c.next = d
+# d.next = e
+# e.next = f
+# # 5 -> 7 -> 10 -> 12 -> 20 -> 28
 
-q = Node(1)
-r = Node(8)
-s = Node(9)
-t = Node(10)
-q.next = r
-r.next = s
-s.next = t
-# 1 -> 8 -> 9 -> 10
+# q = Node(1)
+# r = Node(8)
+# s = Node(9)
+# t = Node(10)
+# q.next = r
+# r.next = s
+# s.next = t
+# # 1 -> 8 -> 9 -> 10
 
-print_ll(merge_lists(a, q))
-# 1 -> 5 -> 7 -> 8 -> 9 -> 10 -> 10 -> 12 -> 20 -> 28 
-
-
+# print_ll(merge_lists(a, q))
+# # 1 -> 5 -> 7 -> 8 -> 9 -> 10 -> 10 -> 12 -> 20 -> 28 
 
 
 
-h = Node(30)
-# 30
 
-p = Node(15)
-q = Node(67)
-p.next = q
-# 15 -> 67
 
-print_ll(merge_lists(h, p))
-# 15 -> 30 -> 67
+# h = Node(30)
+# # 30
+
+# p = Node(15)
+# q = Node(67)
+# p.next = q
+# # 15 -> 67
+
+# print_ll(merge_lists(h, p))
+# # 15 -> 30 -> 67
